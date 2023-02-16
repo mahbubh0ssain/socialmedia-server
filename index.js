376e9278fe4e7ca43cc192d385063e49fa7dd594
Md. Mahbub Hossain <mahbubh0ssain.dev@gmail.com>
2023-02-16 12:48:13 +0600

    MVC added at post routes

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-require("dotenv").config();
-const express = require("express");
-const router = require("./routes");
-const cors = require("cors");
-const port = process.env.PORT || 5000;
-app = express();
-app.use(express.json());
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import router from "./routes/posts.js";
+
+const app = express();
+
+app.use("/posts", router);
+
 app.use(cors());
-app.use(router);
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Social Media server is running successfully");
-});
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Social Media server is running on port ${port}`);
-});
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(port, () => {
+      console.log(`Social Media server is running on port ${port}`);
+    })
+  )
+  .catch((err) => console.log(err?.message));
