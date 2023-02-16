d75be00b19b34db1ea3a023a932d7312de967650
Md. Mahbub Hossain <mahbubh0ssain.dev@gmail.com>
2023-02-16 13:08:41 +0600

    create post

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,7 +1,21 @@
-export const getPosts = (req, res) => {
-  res.send("This is");
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessage = await PostMessage.find();
+    res.send({ success: true, data: postMessage });
+  } catch (err) {
+    res.send({ success: false, message: err?.message });
+  }
 };
 
-export const createPosts = (req, res) => {
-  res.send("This is");
+export const createPosts = async (req, res) => {
+  const post = req.body;
+  const newPost = new PostMessage(post);
+  try {
+    await newPost.save();
+    res.send({ success: true, data: newPost });
+  } catch (err) {
+    res.send({ success: false, message: err?.message });
+  }
 };
